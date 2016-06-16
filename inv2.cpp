bf9f1e5234ff71b03dab313ac60741a6087a0c45
c <chenxinyu@unm.edu>
2016-06-16 00:00:43 -0600

    2 examples to use lapack

---
+++ inv2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+extern "C" {
+    // LU decomoposition of a general matrix
+    void dgetrf_(int* M, int *N, double* A, int* lda, int* IPIV, int* INFO);
+
+    // generate inverse of a matrix given its LU decomposition
+    void dgetri_(int* N, double* A, int* lda, int* IPIV, double* WORK, int* lwork, int* INFO);
+}
+
+void inverse(double* A, int N)
+{
+    int *IPIV = new int[N+1];
+    int LWORK = N*N;
+    double *WORK = new double[LWORK];
+    int INFO;
+
+    dgetrf_(&N,&N,A,&N,IPIV,&INFO);
+    dgetri_(&N,A,&N,IPIV,WORK,&LWORK,&INFO);
+
+    delete IPIV;
+    delete WORK;
+}
+
+int main(){
+
+    double A [2*2] = {
+        1,2,
+        3,4
+    };
+
+    inverse(A, 2);
+
+    printf("%f %f\n", A[0], A[1]);
+    printf("%f %f\n", A[2], A[3]);
+
+    return 0;
+}
